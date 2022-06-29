00fb1a80399f9811695b6aad60cbde2401ff4481
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2022-06-29 10:14:21 -0600

    Added codes in Home, About and contact, Revised App.js to include routers

--- students/src/App.js
+++ students/src/App.js
@@ -1,12 +1,46 @@
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 import StudentList from './components/StudentList';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <StudentList/>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="menu">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About Us</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className="student-container">
+            <StudentList />
+          </div>
+        </div>
+        <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/About' element={< About />}></Route>
+          <Route exact path='/Contact' element={< Contact />}></Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- students/src/components/Student.js
+++ students/src/components/Student.js
@@ -3,9 +3,10 @@ import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
 
 
+
 function Student({ student }) {
     return (
-        <div className='student-container'>        
+        <div>        
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={student.image} width={150} />
             <Card.Body>
