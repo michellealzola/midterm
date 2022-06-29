54f14d9da9051cf4051b90368ab806f473807237
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2022-06-29 08:23:04 -0600

    Revised Student

--- students/src/components/Student.js
+++ students/src/components/Student.js
@@ -5,18 +5,15 @@ import { Card } from 'react-bootstrap';
 function Student({ student }) {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src='NatureData-02.png' width={150} />
+            <Card.Img variant="top" src={student.image} width={150} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Name:{student.name}</Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    GPA: {student.gpa}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">Grade</Button>
             </Card.Body>
         </Card>
-
-
     )
 }
 
